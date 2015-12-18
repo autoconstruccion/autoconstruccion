69a279a09add184fb1730bfac15444b9cc6fbf8c
agustin <agustin.herranz@gmail.com>
2015-12-18 16:46:34 +0100

    Add 403 forbidden error page

--- server_app/autoconstruccion/helpers/helpers.py
+++ server_app/autoconstruccion/helpers/helpers.py
@@ -5,6 +5,12 @@ bp = Blueprint('helpers', __name__)
 log = current_app.logger
 
 
+@bp.app_errorhandler(403)
+def error_internal_server_error(exception):
+    log.error(exception)
+    return make_response(render_template('error/err_403.html'), 403)
+
+
 @bp.app_errorhandler(404)
 def error_page_not_found(exception):
     return make_response(render_template('error/err_404.html'), 404)
